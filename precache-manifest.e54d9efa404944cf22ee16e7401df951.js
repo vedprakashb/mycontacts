386760ab32a369eed607f09fcec22cc5788f6d2e
vedpbehera <vxbehera@sentara.com>
2020-01-11 13:05:15 +0530

    Updates

---
+++ precache-manifest.e54d9efa404944cf22ee16e7401df951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b1039bde7aca7185d269e461ed273f",
+    "url": "/mycontacts/index.html"
+  },
+  {
+    "revision": "a274a4b8179d850b7837",
+    "url": "/mycontacts/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7b142838eb24f12a33a1",
+    "url": "/mycontacts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a274a4b8179d850b7837",
+    "url": "/mycontacts/static/js/2.db7f7148.chunk.js"
+  },
+  {
+    "revision": "b016697e17e4e0dd25a0d4c0374ff03b",
+    "url": "/mycontacts/static/js/2.db7f7148.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b142838eb24f12a33a1",
+    "url": "/mycontacts/static/js/main.79ae222a.chunk.js"
+  },
+  {
+    "revision": "ab993a382e838a1a85a6",
+    "url": "/mycontacts/static/js/runtime-main.930a40a4.js"
+  }
+]);
